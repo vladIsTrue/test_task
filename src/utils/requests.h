2f0dc374827c79d60c3a648e39ef5fc4a7e593fe
vladIsTrue <vladyslav.burtsev@yandex.ru>
2023-10-01 17:28:06 +0300

    add requests.h to folder utils

---
+++ src/utils/requests.h
@@ -0,0 +1,12 @@
+#ifndef REQUESTS_H
+#define REQUESTS_H
+
+enum requestsCode {
+    GET_EMPLOYEE_DATA = 0,
+    GET_ALL_TASKS,
+    GET_EXPIRED_TASKS,
+    INSERT_TASK,
+    REMOVE_TASK
+};
+
+#endif // REQUESTS_H
